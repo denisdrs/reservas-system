7e82fe22a823bc30212967d7245e01d3d153d7ce
joaopaulosg <joaopaulosg17@gmail.com>
2026-02-22 17:43:43 -0300

    feat: Added SecurityConfiguration initial configuration

---
+++ backend/reservas-system/src/test/java/projeto/faculdade/reservas_system/ReservasSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.faculdade.reservas_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
