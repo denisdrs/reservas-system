6c8ef49946f056f5b2c0a97d298944902bb100d0
joaopaulosg <joaopaulosg17@gmail.com>
2026-03-04 16:11:44 -0300

    test: Added register product unit tests

--- backend/reservas-system/src/main/java/projeto/faculdade/reservas_system/presentation/rest/products/contract/RegisterProductRequest.java
+++ backend/reservas-system/src/main/java/projeto/faculdade/reservas_system/presentation/rest/products/contract/RegisterProductRequest.java
@@ -3,9 +3,11 @@ package projeto.faculdade.reservas_system.presentation.rest.products.contract;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 
+@Builder
 public record RegisterProductRequest(
         @NotBlank(message = "Name não pode ser vazio") String name,
         @NotNull(message = "Value não pode ser null") @Positive BigDecimal value,
