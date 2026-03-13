8bc4a7aa88f63c2ae70a8127e52a9249b04da112
joaopaulosg <joaopaulosg17@gmail.com>
2026-03-13 13:00:38 -0300

    feat: Added swagger documentation

--- backend/reservas-system/src/main/java/projeto/faculdade/reservas_system/infra/security/SecurityConfiguration.java
+++ backend/reservas-system/src/main/java/projeto/faculdade/reservas_system/infra/security/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,6 +32,7 @@ public class SecurityConfiguration {
                     authorize.requestMatchers(HttpMethod.POST,"/api/auth/login").permitAll();
                     authorize.requestMatchers(HttpMethod.GET,"/api/products").permitAll();
                     authorize.requestMatchers(HttpMethod.POST,"/api/products").hasAuthority("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "/v3/api-docs*/**").permitAll();
                     authorize.anyRequest().authenticated();
                 })
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
@@ -47,5 +49,11 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs*/**");
     }
 }
