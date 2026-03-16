3fc51a638a5d2fd1a7955f6c258d70307853566a
BeatrizCastro-Dev <beatrizcastrodeveloper@gmail.com>
2026-03-16 16:22:15 -0300

    feat: adicionada página de cadastro e configuração de rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
-const App = () => {
-  return <Login />;
-};
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Rota para o Login */}
+        <Route path="/login" element={<Login />} />
+        
+        {/* Rota para o seu novo Cadastro */}
+        <Route path="/register" element={<Register />} />
+
+        {/* Redireciona qualquer caminho vazio para o login por padrão */}
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
