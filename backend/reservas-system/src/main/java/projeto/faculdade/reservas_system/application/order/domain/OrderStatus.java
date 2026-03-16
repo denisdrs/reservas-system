09f8e359868b95aefe6713b79e23dc101217f53c
joaopaulosg <joaopaulosg17@gmail.com>
2026-03-16 20:09:20 -0300

    feat: Added status in Order, usecase and endpoint

---
+++ backend/reservas-system/src/main/java/projeto/faculdade/reservas_system/application/order/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package projeto.faculdade.reservas_system.application.order.domain;
+
+public enum OrderStatus {
+    PENDENTE, EM_PRODUCAO, ENVIADO, CONCLUIDO
+}
--- backend/reservas-system/src/main/java/projeto/faculdade/reservas_system/application/order/usecase/RegisterNewOrderUseCase.java
+++ backend/reservas-system/src/main/java/projeto/faculdade/reservas_system/application/order/usecase/RegisterNewOrderUseCase.java
@@ -3,12 +3,14 @@ package projeto.faculdade.reservas_system.application.order.usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import projeto.faculdade.reservas_system.application.order.domain.Order;
+import projeto.faculdade.reservas_system.application.order.domain.OrderStatus;
 import projeto.faculdade.reservas_system.application.order.exception.StockException;
 import projeto.faculdade.reservas_system.application.order.port.OrderRepository;
 import projeto.faculdade.reservas_system.application.order.usecase.contract.RegisterNewOrderInput;
 import projeto.faculdade.reservas_system.application.product.domain.Product;
 import projeto.faculdade.reservas_system.application.product.exception.ProductNotFoundException;
 import projeto.faculdade.reservas_system.application.product.port.ProductRepository;
+import projeto.faculdade.reservas_system.application.shared.contract.DefaultMessage;
 import projeto.faculdade.reservas_system.application.user.domain.User;
 
 import java.math.BigDecimal;
@@ -21,7 +23,7 @@ public class RegisterNewOrderUseCase {
 
     private final ProductRepository productRepository;
 
-    public void execute(RegisterNewOrderInput input, User user) {
+    public DefaultMessage<Order> execute(RegisterNewOrderInput input, User user) {
         List<Product> productList = input.products().stream()
                 .map(productInput -> productRepository.findById(productInput.id()))
                 .map(optionalProduct -> optionalProduct.orElseThrow(ProductNotFoundException::new))
@@ -30,9 +32,14 @@ public class RegisterNewOrderUseCase {
                 .products(productList)
                 .user(user)
                 .total(getTotal(productList, input))
+                .status(OrderStatus.PENDENTE)
                 .build();
         orderRepository.save(order);
         removeProductsFromStock(productList, input);
+        return DefaultMessage.<Order>builder()
+                .message("Order successful created")
+                .data(order)
+                .build();
     }
 
     private BigDecimal getTotal(List<Product> productList, RegisterNewOrderInput input) {
